698bf11b713de5cf46ace51580ef56b054ca27af
Nguyen Trung Anh <153763811+hnagnurtme@users.noreply.github.com>
2025-11-20 17:59:12 +0700

    Feat/knowledge create (#69)
    * feat: Implement chatbot query and response handling with HyperClova integration
    
    * feat: Add CopyNodesAsync method to IKnowledgetreeRepository for cloning nodes across workspaces
    
    * Refactor documentation and API specifications
    
    - Removed outdated flow documentation from `docs/flow.md`.
    - Added new API endpoint for chatbot queries in `docs/swagger.json`.
    - Updated response schemas and added new components for chatbot functionality in `swagger.json`.
    - Deleted obsolete test structure files `test_structure_1.json` and `test_structure_2.json`.
    
    * feat: Add documentation for optimizing RabbitMQ Worker and enhancing frontend UX for Knowledge Graph Build

--- Backend/NavNexus.API/Contract/KnowledgeTree/RabbitMqSendingResponse.cs
+++ Backend/NavNexus.API/Contract/KnowledgeTree/RabbitMqSendingResponse.cs
@@ -3,11 +3,18 @@ namespace NavNexus.API.Contract.KnowledgeTree;
 public class RabbitMqSendingResponse
 {
     public required string MessageId { get; set; }
+
+    public required string WorkspaceId { get; set; } = string.Empty;
+
+    public required string Status { get; set; } = "PENDING"; // PENDING , SUCCESS
+
     public required DateTime SentAt { get; set; }
 
-    public RabbitMqSendingResponse(string messageId, DateTime sentAt)
+    public RabbitMqSendingResponse(string messageId, string workspaceId, DateTime sentAt, string status = "PENDING")
     {
         MessageId = messageId;
         SentAt = sentAt;
+        WorkspaceId = workspaceId;
+        Status = status;
     }
 }
--- Backend/NavNexus.Application/KnowledgeTree/Results/RabbitMqSendingResult.cs
+++ Backend/NavNexus.Application/KnowledgeTree/Results/RabbitMqSendingResult.cs
@@ -1,14 +1,22 @@
+using Google.Rpc;
+
 namespace NavNexus.Application.KnowledgeTree.Results;
 
 public class RabbitMqSendingResult
 {
     public string MessageId { get; set; }
 
+    public string WorkspaceId { get; set; } = string.Empty;
+
+    public string Status { get; set; } = "PENDING"; // PENDING , SUCCESS
+
     public  DateTime SentAt { get; set; }
 
-    public RabbitMqSendingResult(string messageId, DateTime sentAt)
+    public RabbitMqSendingResult(string messageId, string workspaceId, DateTime sentAt, string status = "PENDING")
     {
         MessageId = messageId;
         SentAt = sentAt;
+        WorkspaceId = workspaceId;
+        Status = status;
     }
 }
